bd790b5196072ed5d485f9efa4f08cf52ab3f97d
nozomi-takuma <nozomi.takuma@dena.com>
2021-09-21 13:20:34 +0900

    ライブラリ単独で動作確認ができるようにサンプルのテストを追加

---
+++ library/src/androidTest/java/app/mobilitytechnologies/uitest/CustomAndroidJUnitRunner.kt
@@ -0,0 +1,18 @@
+package app.mobilitytechnologies.uitest
+
+import android.os.Bundle
+import androidx.test.runner.AndroidJUnitRunner
+import app.mobilitytechnologies.uitest.snapshot.SnapShot
+
+class CustomAndroidJUnitRunner : AndroidJUnitRunner() {
+    override fun onCreate(arguments: Bundle?) {
+        val newArguments = UiTestRunListener.appendListenerArgument(arguments)
+        super.onCreate(newArguments)
+    }
+
+    override fun finish(resultCode: Int, results: Bundle?) {
+        SnapShot.zipAll()
+        super.finish(resultCode, results)
+        UiTestDeveloperSettings.onInstrumentationFinished()
+    }
+}
